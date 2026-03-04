fa50cd5f8a019be850a15942a9ec3050d661748d
HdEX <doriankch@gmail.com>
2026-03-04 08:58:54 +0100

    correction de dice.py

--- app/dice.py
+++ app/dice.py
@@ -1,10 +1,11 @@
 from random import randint
 
 class Dice:
+    @staticmethod
     def lancer(nb, montant):
         score = 0
         for e in range(nb-1):
             lancer = randint(1,montant)
             score += lancer
-            print(lancer)
+            print(f"Lancé du dé : {lancer}")
         return score
