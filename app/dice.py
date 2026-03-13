e2b2d06807ff498a8c7db4ddc7b0b2a34ece8c24
HdEX <doriankch@gmail.com>
2026-03-13 09:24:35 +0100

    deuxieme correction de dice.py + docstring

--- app/dice.py
+++ app/dice.py
@@ -3,9 +3,18 @@ from random import randint
 class Dice:
     @staticmethod
     def lancer(nb, montant):
+        """
+        Lancer de dé
+        Paramètres : nb (int) -> nombre de lancers / montant (int) -> nombre de faces du dé
+        Sortie : Renvoie un tuple contenant le score total des lancers et une liste contenant le résultat de chaque lancer indépendament
+        Initialisez une variable et assignez-lui Dice.lancer(nb,montant) pour lancer autant de dés que vous voulez
+        Votre variable contiendra le score total
+        """
+        resultats = []
         score = 0
-        for e in range(nb-1):
+        for e in range(nb):
             lancer = randint(1,montant)
             score += lancer
+            resultats.append(lancer)
             print(f"Lancé du dé : {lancer}")
-        return score
+        return (score, resultats)
