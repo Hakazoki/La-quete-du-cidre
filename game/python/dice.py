124de5603c0f81134a458eca05fe597f36fe9e06
HdEX <doriankch@gmail.com>
2026-03-16 10:59:13 +0100

    commit dice pour merge

--- game/python/dice.py
+++ game/python/dice.py
@@ -2,17 +2,17 @@ from random import randint
 
 class Dice:
     @staticmethod
-    def lancer(nb, montant):
+    def lancer( nb = 1, nb_faces=20):
         """
         Lancer de dé
-        Paramètres : nb (int) -> nombre de lancers / montant (int) -> nombre de faces du dé
+        Paramètres : nb (int) -> nombre de lancers / nb_faces (int) -> nombre de faces du dé
         Sortie : Renvoie une liste contenant le score total des lancers et une liste contenant le résultat de chaque lancer indépendament
         Initialisez une variable et assignez-lui Dice.lancer(nb,montant) pour lancer autant de dés que vous voulez
         """
         resultats = []
         score = 0
         for e in range(nb):
-            lancer = randint(1,montant)
+            lancer = randint(1,nb_faces)
             score += lancer
             resultats.append(lancer)
             print(f"Lancé du dé : {lancer}")
